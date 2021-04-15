1b24df6b82635558e1482024b82dc5be4d756a7f
liuql <liuql@usoftchina.com>
2021-04-15 17:55:12 +0800

    提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,159 +1,36 @@
-
+// import barObj from './config/sideBar'
+const barObj = require('./config/sideBar.js')
 module.exports = {
-  description: 'this is onesugar web',
+  description: '专注于日常学习与总结。',
   title: "Arlene",
-  description: "专注于日常学习与总结。",
   // 记录在工作中遇到的一些问题的解决方案以及一些新知识，新技术，和在生活中的一些教育相关问题.
   base: '/',
   themeConfig: {
+    title: "Arlene",
+    logo: '/logo.jpg',
     editLinks: true,
     docsDir: 'docs',
     displayAllHeaders: true,
     nav: [
       { text: '首页', link: '/' },
-      { text: '前端', link: '/tech/' },
-      { text: '教育', link: '/edu/', target: '_blank', },
+      {
+        text: '前端',
+        ariaLabel: '前端技术',
+        items: [
+          { text: 'JS基础', link: '/tech/js/' },
+          { text: 'TypeScript', link: '/tech/ts/' },
+          { text: 'Vue3', link: '/tech/vueNext/' },
+          { text: 'React', link: '/tech/react/' },
+          { text: '博客', link: '/ts-axios/chapter1/' },
+        ]
+      },
+      { text: '教育', link: '/edu/part1/', target: '_blank', },
       { text: 'ts-axios', link: '/ts-axios/chapter1/' },
     ],
-    sidebar: {
-      '/ts-axios/': getTsAxiosSidebar()
-    },
+    sidebar: barObj,
     extraWatchFiles: [
       '/docs'
     ]
   },
   dest: './docs/.vuepress/dist',
-}
-function getTsAxiosSidebar () {
-  return [
-    {
-      title: '初识 TypeScript',
-      collapsable: false,
-      children: [
-        ['chapter1/', 'Introduction'],
-        'chapter1/install',
-        'chapter1/start'
-      ]
-    },
-    {
-      title: 'TypeScript 常用语法',
-      collapsable: false,
-      children: [
-        'chapter2/type',
-        'chapter2/declare',
-        'chapter2/interface',
-        'chapter2/class',
-        'chapter2/function',
-        'chapter2/generic',
-        'chapter2/inference',
-        'chapter2/advance'
-      ]
-    },
-    {
-      title: 'ts-axios 项目初始化',
-      collapsable: false,
-      children: [
-        'chapter3/require',
-        'chapter3/init',
-        'chapter3/base'
-      ]
-    },
-    {
-      title: 'ts-axios 基础功能实现',
-      collapsable: false,
-      children: [
-        'chapter4/url',
-        'chapter4/data',
-        'chapter4/header',
-        'chapter4/response',
-        'chapter4/response-header',
-        'chapter4/response-data'
-      ]
-    },
-    {
-      title: 'ts-axios 异常情况处理',
-      collapsable: false,
-      children: [
-        'chapter5/error',
-        'chapter5/enhance'
-      ]
-    },
-    {
-      title: 'ts-axios 接口扩展',
-      collapsable: false,
-      children: [
-        'chapter6/extend',
-        'chapter6/overload',
-        'chapter6/generic'
-      ]
-    },
-    {
-      title: 'ts-axios 拦截器实现',
-      collapsable: false,
-      children: [
-        'chapter7/interceptor'
-      ]
-    },
-    {
-      title: 'ts-axios 配置化实现',
-      collapsable: false,
-      children: [
-        'chapter8/merge',
-        'chapter8/transform',
-        'chapter8/create'
-      ]
-    },
-    {
-      title: 'ts-axios 取消功能实现',
-      collapsable: false,
-      children: [
-        'chapter9/cancel'
-      ]
-    },
-    {
-      title: 'ts-axios 更多功能实现',
-      collapsable: false,
-      children: [
-        'chapter10/withCredentials',
-        'chapter10/xsrf',
-        'chapter10/upload-download',
-        'chapter10/auth',
-        'chapter10/validateStatus',
-        'chapter10/paramsSerializer',
-        'chapter10/baseURL',
-        'chapter10/static'
-      ]
-    },
-    {
-      title: 'ts-axios 单元测试',
-      collapsable: false,
-      children: [
-        'chapter11/preface',
-        'chapter11/jest',
-        'chapter11/helpers',
-        'chapter11/requests',
-        'chapter11/headers',
-        'chapter11/instance',
-        'chapter11/interceptor',
-        'chapter11/mergeConfig',
-        'chapter11/cancel',
-        'chapter11/more'
-      ]
-    },
-    {
-      title: 'ts-axios 部署与发布',
-      collapsable: false,
-      children: [
-        'chapter12/build-deploy',
-        'chapter12/demo'
-      ]
-    },
-    {
-      title: '课程总结',
-      collapsable: false,
-      children: [
-        'chapter13/summary'
-      ]
-    }
-  ]
 }
--- docs/README.md
+++ docs/README.md
@@ -4,14 +4,18 @@ home: true
 footer: '@2021 Arlene | 个人网站 | 鄂ICP备2020018075号'
 ---
 <div class="base">
- <div v-for="(item,index) in lists" class="item" :key="index">{{item.name}}</div>
+ <a v-for="(item,index) in lists" class="item" :key="index" :href="item.link">{{item.name}}</a>
 </div>
 
 <script>
  export default {
   data(){
     return {
-      lists:[{name:'js基础',link:'/'},{name:'TypeScript'},{name:'Vue3'},{name:'React'},{name:'博客'},]
+      lists:[{name:'js基础',link:'/tech/js/chapter1/'},
+      {name:'TypeScript',link:'/tech/ts/chapter1/'},
+      {name:'Vue3',link:'/tech/vueNext/chapter1/'},
+      {name:'React',link:'/tech/react/chapter1/'},
+      {name:'博客',link:'/ts-axios/chapter1/'},]
     }
   }
  }
